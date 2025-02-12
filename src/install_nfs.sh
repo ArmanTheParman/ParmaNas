25f081c466ed0a4c70ba4ffd05f4bc0cd944612c
Parman <armantheparman@protonmail.com>
2025-02-13 02:50:44 +1100

    a

--- src/install_nfs.sh
+++ src/install_nfs.sh
@@ -47,14 +47,10 @@ choose xq ; read choice
 case $choice in
 q|Q) exit ;; p|P) return 1 ;;
 all) 
-
 IP_1=$(echo "$IP" | cut -d \. -f 1)
 IP_2=$(echo "$IP" | cut -d \. -f 2)
 IP_3=$(echo "$IP" | cut -d \. -f 3)
-
 allowed_IP="$IP_1.$IP_2.$IP_3.0/24"
-
-
 break
 ;;
 one) 
@@ -62,6 +58,7 @@ one)
     echo -e "${blue}Please enter the IP address you want, then hit $orange<enter>$blue\n"
     read allowed_IP 
     yesorno "Go ahead with this IP address? $allowed_IP" || continue
+    export allowed_IP
     break
     ;;
 *) invalid ;;
