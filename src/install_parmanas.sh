c33f77eec0b9245005764c4b77ac7163e89147e6
Parman <armantheparman@protonmail.com>
2025-02-12 18:31:06 +1100

    a

--- src/install_parmanas.sh
+++ src/install_parmanas.sh
@@ -6,10 +6,19 @@ nasuser=parmanasuser
 
 nas_intro_and_choice || return 1
 
-make_parmanas_directories || return 1
-
-
 #make users
 sudo adduser $nasuser
+
+make_parmanas_directories || return 1
+installed_conf_add "nas-start"
+
+if [[ $install == "samba" ]] ; then 
+    install_samba
+    parmanode_conf_add "nas=samba"
+else 
+    install_nfs
+    parmanode_conf_add "nas=nfs"
+fi
+
 
 }
